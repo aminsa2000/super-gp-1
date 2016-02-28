c350fa2069d67772c188cfd2d38b9fe683960424
Whitewolf2 <whitewolforg5@gmail.com>
2016-02-28 22:06:10 +0330

    Update super_nod32.lua

--- bot/super_nod32.lua
+++ bot/super_nod32.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {184111248},
+    sudo_users = {134442744,1165472},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
